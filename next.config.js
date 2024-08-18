6bcd8e53a7760310f85fc59111ecfbf750101b16
Swastik Kulshreshtha <56737344+Swastik2442@users.noreply.github.com>
2024-08-18 12:51:32 +0530

    Build Process Changes

--- next.config.js
+++ next.config.js
@@ -5,6 +5,13 @@
 await import("./src/env.js");
 
 /** @type {import("next").NextConfig} */
-const config = {};
+const config = {
+    typescript: {
+        ignoreBuildErrors: true, // Useful for separate Error Checking
+    },
+    eslint: {
+        ignoreDuringBuilds: true,
+    },
+};
 
 export default config;
